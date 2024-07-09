53b2d4c8f78bf53ef83d0512b1948a7c60fede1b
ttimot24 <ttimotster@gmail.com>
2024-07-09 20:08:23 +0200

    Upgrade to bootstrap 5.3

--- app/View/index.blade.php
+++ app/View/index.blade.php
@@ -5,28 +5,36 @@
 <link rel="stylesheet" type="text/css" href="plugins/GoogleMaps/resources/style.css">
 
 <section class='container'>
+  
 
+  <div class='card'>
+
+    <div class='card-body'>
 <div class='row'>
-<h1>{{$self->getName()}} <small>v{{$self->getInfo('version','')}}</small></h1><br>
+ <h1 class="my-4">{{$self->getName()}} <small>v{{$self->getInfo('version','')}}</small></h1><br>
 </div>
 
-
-<div class='col-md-8'>
+<div class="row">
+<div class='col-8'>
 
   <!-- Nav tabs -->
-  <div class='well'>
   <ul class="nav nav-tabs" role="tablist">
-    <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">{{trans('plugin::nav.home')}}</a></li>
-    <li role="presentation"><a href="#new_location" aria-controls="nre_location" role="tab" data-toggle="tab">{{trans('plugin::nav.add_location')}}</a></li>
-    <li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">{{trans('plugin::nav.settings')}}</a></li>
-
-    <li role="presentation" class="pull-right"><a href="#docs" aria-controls="docs" role="tab" data-toggle="tab">{{trans('plugin::nav.docs')}}</a></li>
+    <li role="presentation" class="nav-item active">
+      <a class="nav-link" href="#home" aria-controls="home" role="tab" data-bs-toggle="tab">{{trans('plugin::nav.home')}}</a></li>
+    <li role="presentation" class="nav-item">
+      <a class="nav-link" href="#new_location" aria-controls="nre_location" role="tab" data-bs-toggle="tab">{{trans('plugin::nav.add_location')}}</a>
+    </li>
+    <li role="presentation" class="nav-item">
+      <a class="nav-link"href="#settings" aria-controls="settings" role="tab" data-bs-toggle="tab">{{trans('plugin::nav.settings')}}</a>
+    </li>
+    <li class="nav-item" role="presentation">
+      <a class="nav-link" href="#docs" aria-controls="docs" role="tab" data-bs-toggle="tab">{{trans('plugin::nav.docs')}}</a>
+    </li>
   
   </ul>
-  </div>
 
   <!-- Tab panes -->
-  <div class="tab-content">
+  <div class="tab-content py-3">
     <div role="tabpanel" class="tab-pane active" id="home">@include('plugin::list')</div>
     <div role="tabpanel" class="tab-pane" id="new_location">@include('plugin::create_location')</div>
     <div role="tabpanel" class="tab-pane" id="settings">@include('plugin::settings')</div>
@@ -36,12 +44,14 @@
 
 </div>
 
-<div class='col-md-4' style='text-align:right;'>
+<div class='col-4' style='text-align:right;'>
   <div id="map_preview">
       @include('plugin::widget.widget')
   </div>
 </div>
+</div>
 
-
+</div>
+</div>
  </section>
  @endsection
